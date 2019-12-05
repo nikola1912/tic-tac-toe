41f85e7ef3760b1c1506e48089aa984b88ce4c52
Nikola Jovanovic <nikola.jov1912@gmail.com>
2019-12-05 20:08:01 +0100

    Implement win condition functions for row and column

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const gameBoard = (function() {
-
-    //let _board = ["x","x","x","x","x","x","x","o","o"];
-    let board = ["","","","","","","","",""];
+    
+    let board = ["1","2","3","4","5","6","7","8","9"];
+    //let board = ["","","","","","","","",""];
     let _currentPlayer = "&#10005;";
     let _Xmarker = "&#10005;";
     let _Omarker = "&#9711;";
 
     const  _togglePlayerMarker = () => {
         // Changes current player marker to the opposite marker - X to O || O to X
-        _currentPlayer == _Xmarker ? _currentPlayer = _Omarker : _currentPlayer = _Xmarker;
+        _currentPlayer = _currentPlayer == _Xmarker ? _Omarker : _Xmarker;
     };
 
     const _isCellEmpty = (cell) => {
@@ -18,15 +18,61 @@ const gameBoard = (function() {
 
     const _fillBoardWithMarker = (marker, index) => {
         // Sets the given 'board' array index value to the given marker 
-        marker == _Xmarker ? board[index] = "x" : board[index] = "o";
+        board[index] = marker == _Xmarker ? "x" : "o";
+    };
+
+    const _getMarkerValue = (marker) => {
+        // Returns given marker as string "✕" or "◯"
+        return marker == _Xmarker ? "✕" : "◯";
+    }
+
+    const _isBoardFilled = () => {
+        // Checks if the board is completely filled
+        return board.every(cell => cell != "");
+    };
+
+    const _isMarkerThirdInRow = (cell, marker) => {
+        // Checks if the given cells row is filled with the same marker
+        let markerHTMLsign = _getMarkerValue(marker); // "✕" or "◯"
+        let cellsInSameRow = document.getElementsByClassName(cell.classList[1]);
+        for (let cell of cellsInSameRow) {
+            if (cell.innerHTML != markerHTMLsign) {return false;}
+        }
+        console.log(`Player ${markerHTMLsign} WINS!`);
+        return true;
+    }
+
+    const _isMarkerThirdInColumn = (cell, marker) => {
+        let markerHTMLsign = _getMarkerValue(marker); // "✕" or "◯"
+        let cellsInSameColumn = document.getElementsByClassName(cell.classList[2]);
+        for (let cell of cellsInSameColumn) {
+            if (cell.innerHTML != markerHTMLsign) {return false;}
+        }
+        console.log(`Player ${markerHTMLsign} WINS!`);
+        return true;
+    }
+
+    const _isMarkerThirdInDiagonal = (cell, marker) => {
+
+    };
+
+    const _isMoveWinner = (cell, marker) => {
+        // Checks if the cell is connected by: row, column or diagonal, with 2 cells that have the same marker
+        return _isMarkerThirdInRow(cell, marker) || _isMarkerThirdInColumn(cell, marker) || _isMarkerThirdInDiagonal(cell, marker);
+    }
+
+    const _isGameOver = (cell, marker) => {
+        // Checks if there are 3 markers in a row or if the game is tied
+        return _isMoveWinner(cell, marker) || _isBoardFilled();
     };
 
     const handlePlayerInput = (event) => {
-        // Draws marker on current cell if it's empty and switches the current player
+        // Draws marker on current cell if it's empty and switches the current player, then checks if the game is over
         let cell = event.target; // The cell that was clicked on
         if (_isCellEmpty(cell)) {
             displayController.drawMarkerOnCell(cell, _currentPlayer);
             _fillBoardWithMarker(_currentPlayer, Number(cell.id));
+            _isGameOver(cell, _currentPlayer);
             _togglePlayerMarker();
         }
         console.log(event);
@@ -67,5 +113,5 @@ const player = (marker) => {
 let playerX = player("&#10005;");
 let playerO = player("&#9711;");
 
-document.getElementById("gameBody").addEventListener("click", gameBoard.handlePlayerInput);
+document.getElementById("game").addEventListener("click", gameBoard.handlePlayerInput);
 displayController.drawBoard();
